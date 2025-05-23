dec4c0e216a52341124afeb0c1be834ad7355af2
Joost Meijles <joost.meijles@avivasolutions.nl>
2025-05-23 10:31:35 +0200

    Remove pages

--- pages/500.tsx
+++
@@ -1,5 +0,0 @@
-// The pages router has no support for logging the specific error that occurred.
-// See https://nextjs.org/docs/pages/building-your-application/routing/custom-error
-export default function Error() {
-  return <div>Error</div>;
-}
