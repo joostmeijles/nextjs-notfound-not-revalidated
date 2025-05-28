74417d8f2e5cf02d4b694c04a8d035189d55cc6d
Joost Meijles <joost.meijles@avivasolutions.nl>
2025-05-28 15:00:06 +0200

    Call notFound from within catch-all

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,21 +1,7 @@
+import { notFound } from "next/navigation";
+
 export const dynamic = 'force-static';
 
 export default async function Page() {
-  const date = Date.now();
-  const id = date % 500;
-  const res = await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {
-    next: {
-      tags: ['example-data'], // Attach the revalidate tag
-    },
-  });
-
-  const data = await res.json();
-
-  return (
-    <html lang="en">
-      <body>
-        <div>NOTFOUND: {date} - {id} - {data?.name}</div>
-      </body>
-    </html>
-  );
+  notFound();
 }
