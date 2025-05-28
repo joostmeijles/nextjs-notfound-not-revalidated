1427d89b810372e670017981f1a0e710c37e7076
Joost Meijles <joost.meijles@avivasolutions.nl>
2025-05-28 14:16:49 +0200

    Use catchall iso not-found

---
+++ app/[...slug]/page.tsx
@@ -0,0 +1,20 @@
+export default async function Page() {
+  const date = Date.now();
+  const id = date % 500;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {
+    next: {
+      tags: ['example-data'], // Attach the revalidate tag
+      revalidate: 10
+    },
+  });
+
+  const data = await res.json();
+
+  return (
+    <html lang="en">
+      <body>
+        <div>NOTFOUND: {date} - {id} - {data?.name}</div>
+      </body>
+    </html>
+  );
+}
