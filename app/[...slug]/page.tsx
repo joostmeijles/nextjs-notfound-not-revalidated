75ed9c2e9de9d54086249e69b2e3db64437f6efc
Joost Meijles <joost.meijles@avivasolutions.nl>
2025-05-28 14:53:04 +0200

    Remove revalidate time

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -6,7 +6,6 @@ export default async function Page() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {
     next: {
       tags: ['example-data'], // Attach the revalidate tag
-      revalidate: 10
     },
   });
 
